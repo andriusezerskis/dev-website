42938a6e97a5b573bbdfece8bdb2301ade8584f8
andriusezerskis <andriuko6@gmail.com>
2024-09-03 22:59:09 +0200

    site pretty much done, only socials left and hosting

--- scripts/header_import.js
+++ scripts/header_import.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Import header
     fetch('main_header.html')
         .then(response => response.text())
         .then(data => {
--- scripts/script_timeline.js
+++ scripts/script_timeline.js
@@ -1,13 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const timelineItems = document.querySelectorAll('.timeline-item');
 
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
             }
         });
-    });
+    }, { threshold: 0.1 });
 
     timelineItems.forEach(item => {
         observer.observe(item);
