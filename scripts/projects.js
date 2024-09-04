bfe250757619b5ab29a24ac817f42f796d3d841f
andriusezerskis <andriuko6@gmail.com>
2024-09-05 00:16:36 +0200

    :recycle: modified technology icons and filters

--- scripts/projects.js
+++ scripts/projects.js
@@ -4,36 +4,36 @@ document.addEventListener('DOMContentLoaded', () => {
             title: "Load balancer for a website",
             description: "Implementation of a load balancer to handle traffic and send it across different webservers, to ensure the stability of the website.",
             link: "https://github.com/moiravs/LoadBalancer",
-            technologies: ["nginx", "php", "database", "python"]
+            technologies: ["nginx", "php", "mysql", "python"]
         },
         {
             title: "Breakthrough game",
             description: "Simplified chess game with Minimax and Monte-Carlo algorithms to play against AI.",
-            technologies: ["PyQt5", "python"]
+            technologies: ["qt", "python"]
         },
         {
             title: "TinyDB and SmallDB",
             description: "Implementation of a C/C++ database with processes and threads for handling search queries with filters.",
             link: "https://github.com/moiravs/TinyDB",
-            technologies: ["Bash", "cuttlefish", "database", "terminal"]
+            technologies: ["bash", "c", "cplusplus", "mysql"]
         },
         {
             title: "Sokoban game",
             description: "Implementation of the Sokoban game in Object-Oriented C++.",
             link: "https://github.com/moiravs/Sokoban",
-            technologies: ["FLTK", "c++"]
+            technologies: ["fltk", "cplusplus"]
         },
         {
             title: "Patients' information database",
             description: "Creation of a database containing patient informations, such as their doctor and pharmacist, to insure proper care following.",
             link: "https://github.com/andriusezerskis/ProjetBDD",
-            technologies: ["php", "database", "python"]
+            technologies: ["php", "mysql", "python"]
         },
         {
             title: "2D Ecosystem simulation",
             description: "Simulation of a 2D environment containing animals, humans and different plants interacting with each other in a stochastic manner.",
             link: "https://github.com/andriusezerskis/proj3-gr2b",
-            technologies: ["PyQt6", "python"]
+            technologies: ["qt", "python"]
         }
     ];
 
@@ -80,7 +80,7 @@ document.addEventListener('DOMContentLoaded', () => {
         } else { var filteredProjects = projects; }
         filteredProjects.forEach((project, index) => {
             const isActive = index === 0 ? 'active' : '';
-            const technologies = project.technologies.map(tech => `<i class="fab fa-${tech}"></i>`).join(' ');
+            const technologies = project.technologies.map(tech => `<i class="devicon-${tech}-plain"></i>`).join(' ');
             const projectCard = `
                 <div class="carousel-item ${isActive}">
                     <div class="card mb-4 white-shadow bg-dark-gray text-light">
@@ -99,26 +99,18 @@ document.addEventListener('DOMContentLoaded', () => {
     projects.forEach((project, index) => {
         const isActive = index === 0 ? 'active' : '';
         const technologies = project.technologies.map(tech => {
-            if (tech === 'cuttlefish' || tech === 'c++') {
-                return `<i class="fab fa-cuttlefish">++</i>`;
-            } else if (tech === 'nginx') {
-                return `<img class="nginx-icon" src='../images/nginx.svg' alt='NGinx'>`;
-            } else if (tech === 'FLTK' || tech === 'PyQt6' || tech === 'PyQt5' || tech === 'Bash') {
-                return `<a>${tech}</a>`;
-            } else if (tech === 'database') {
-                return `<i class="fas fa-database"></i>`;
-            } else if (tech === 'terminal') {
-                return `<i class="fa fa-terminal"></i>`;
+            if (tech === 'fltk') {
+                return '<a>fltk</a>';
             } else {
-                return `<i class="fab fa-${tech}"></i>`;
+                return `<i class="devicon-${tech}-plain"></i>`;
             }
         }).join(' ');
-
         const projectCard = `
             <div class="carousel-item ${isActive}">
                 <div class="card mb-4 white-shadow bg-dark-gray text-light">
                     <div class="card-body">
                         <h5 class="card-title text-hover">${project.title} ${project.link ? `<a href="${project.link}" target="_blank" class="bi bi-github"></a>` : ''}</h5>
+          
                         <p class="card-text">${project.description}</p>
                         <div class="technologies">${technologies}</div>
                     </div>
