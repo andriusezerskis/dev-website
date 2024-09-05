c543b13984fa5c93ef3f94dab71299057a05a318
andriusezerskis <andriuko6@gmail.com>
2024-09-06 00:01:53 +0200

    :construction: working on the filter menu

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,158 +1,177 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const projects = [
-        {
-            title: "Load balancer for a website",
-            description: "Implementation of a load balancer to handle traffic and send it across different webservers, to ensure the stability of the website.",
-            link: "https://github.com/moiravs/LoadBalancer",
-            technologies: ["nginx", "php", "mysql", "python"],
-            filters: ["NGinx", "PHP", "MySQL", "Python"]
-        },
-        {
-            title: "Breakthrough game",
-            description: "Simplified chess game with Minimax and Monte-Carlo algorithms to play against AI.",
-            technologies: ["qt", "python"],
-            filters: ["Qt", "Python"]
-        },
-        {
-            title: "TinyDB and SmallDB",
-            description: "Implementation of a C/C++ database with processes and threads for handling search queries with filters.",
-            link: "https://github.com/moiravs/TinyDB",
-            technologies: ["bash", "c", "cplusplus", "mysql"],
-            filters: ["Bash", "C", "C++", "MySQL"]
-        },
-        {
-            title: "Sokoban game",
-            description: "Implementation of the Sokoban game in Object-Oriented C++.",
-            link: "https://github.com/moiravs/Sokoban",
-            technologies: ["fltk", "cplusplus"],
-            filters: ["FLTK", "C++"]
-        },
-        {
-            title: "Patients' information database",
-            description: "Creation of a database containing patient informations, such as their doctor and pharmacist, to insure proper care following.",
-            link: "https://github.com/andriusezerskis/ProjetBDD",
-            technologies: ["php", "mysql", "python"],
-            filters: ["PHP", "MySQL", "Python"]
-        },
-        {
-            title: "2D Ecosystem simulation",
-            description: "Simulation of a 2D environment containing animals, humans and different plants interacting with each other in a stochastic manner.",
-            link: "https://github.com/andriusezerskis/proj3-gr2b",
-            technologies: ["qt", "python"],
-            filters: ["Qt", "Python"]
-        }
-    ];
+const carouselInner = document.getElementById("carousel-inner");
+let activeTechs = [];
 
-    // Extract unique technologies
-    const technologies = new Set();
-    projects.forEach(project => {
-        project.technologies.forEach(tech => technologies.add(tech));
-    });
+// Function to filter projects
+function filterProjects(activeTechs) {
+	carouselInner.innerHTML = ""; // Clear existing projects
+	if (activeTechs.length > 0) {
+		var filteredProjects = projects.filter((project) => {
+			return project.technologies.some((tech) => activeTechs.includes(tech));
+		});
+	} else {
+		var filteredProjects = projects;
+	}
 
-    const filterButtonsContainer = document.getElementById('filter-buttons');
-    const carouselInner = document.getElementById('carousel-inner');
-    let activeTechs = [];
-
-    const filtersMapping = {};
-    projects.forEach(project => {
-        project.technologies.forEach((tech, index) => {
-            filtersMapping[tech] = project.filters[index];
-        });
-    });
-
-    // Generate filter buttons
-    technologies.forEach(tech => {
-        const button = document.createElement('button');
-        button.className = 'btn btn-secondary btn-toggle m-1';
-        button.innerText = filtersMapping[tech];
-        button.addEventListener('click', () => toggleFilter(tech, button));
-        button.addEventListener('click', function() {
-            this.classList.toggle('active');
-        });
-        filterButtonsContainer.appendChild(button);
-    });
-
-    // Create and append the "Clear Filters" button
-    const clearButton = document.createElement('button');
-    clearButton.className = 'btn btn-danger m-1';
-    clearButton.innerText = 'Clear Filters';
-    clearButton.addEventListener('click', clearFilters);
-    filterButtonsContainer.appendChild(clearButton);
-
-    // Function to clear all filters
-    function clearFilters() {
-        activeTechs = [];
-        document.querySelectorAll('#filter-buttons .btn-toggle').forEach(btn => {
-            btn.classList.remove('active');
-        });
-        filterProjects(activeTechs);
-    }
-
-    // Function to toggle filter
-    function toggleFilter(tech, button) {
-        if (activeTechs.includes(tech)) {
-            // If the same button is clicked again, remove it from the list of active techs
-            activeTechs = activeTechs.filter(t => t !== tech);
-        } else {
-            // If a different button is clicked, add it to the list of active techs
-            activeTechs.push(tech);
-        }
-        filterProjects(activeTechs);
-    }
-
-    // Function to filter projects
-    function filterProjects(activeTechs) {
-        carouselInner.innerHTML = ''; // Clear existing projects
-        if (activeTechs.length > 0) {
-            var filteredProjects = projects.filter(project => {
-                return project.technologies.some(tech => activeTechs.includes(tech));
-            });
-        } else {
-            var filteredProjects = projects;
-        }
-        filteredProjects.forEach((project, index) => {
-            const isActive = index === 0 ? 'active' : '';
-            const technologies = project.technologies.map(tech => `<i class="devicon-${tech}-plain"></i>`).join(' ');
-            const projectCard = `
-                <div class="carousel-item ${isActive}">
-                    <div class="card mb-4 white-shadow bg-dark-gray text-light">
-                        <div class="card-body">
-                            <h5 class="card-title text-hover">${project.title} ${project.link ? `<a href="${project.link}" target="_blank" class="bi bi-github"></a>` : ''}</h5>
-                            <p class="card-text">${project.description}</p>
-                            <div class="technologies">${technologies}</div>
-                        </div>
-                    </div>
-                </div>
-            `;
-            carouselInner.insertAdjacentHTML('beforeend', projectCard);
-        });
-    }
-
-    projects.forEach((project, index) => {
-        const isActive = index === 0 ? 'active' : '';
-        const technologies = project.technologies.map(tech => {
-            if (tech === 'fltk') {
-                return '<a>fltk</a>';
-            } else {
-                return `<i class="devicon-${tech}-plain"></i>`;
-            }
-        }).join(' ');
-        const projectCard = `
-            <div class="carousel-item ${isActive}">
-                <div class="card mb-4 white-shadow bg-dark-gray text-light">
-                    <div class="card-body">
-                        <h5 class="card-title text-hover">${project.title} ${project.link ? `<a href="${project.link}" target="_blank" class="bi bi-github"></a>` : ''}</h5>
-          
-                        <p class="card-text">${project.description}</p>
-                        <div class="technologies">${technologies}</div>
-                    </div>
+	filteredProjects.forEach((project, index) => {
+		const isActive = index === 0 ? "active" : "";
+		const technologies = project.technologies
+			.map((tech) => `<i class="devicon-${tech}-plain"></i>`)
+			.join(" ");
+		const projectCard = `
+        <div class="carousel-item ${isActive}">
+            <div class="card mb-4 white-shadow bg-dark-gray text-light">
+                <div class="card-body">
+                    <h5 class="card-title text-hover">${project.title} ${
+			project.link
+				? `<a href="${project.link}" target="_blank" class="bi bi-github"></a>`
+				: ""
+		}</h5>
+                    <p class="card-text">${project.description}</p>
+                    <div class="technologies">${technologies}</div>
                 </div>
             </div>
-        `;
+        </div>
+    `;
+		carouselInner.insertAdjacentHTML("beforeend", projectCard);
+	});
+}
 
-        carouselInner.insertAdjacentHTML('beforeend', projectCard);
-    });
+// Function to clear all filters
+function clearFilters() {
+	activeTechs = [];
+	document
+		.querySelectorAll("#filter-buttons .dropdown-item .btn-toggle")
+		.forEach((btn) => {
+			btn.classList.remove("active");
+		});
+	filterProjects(activeTechs);
+}
 
-    // Initially display all projects
-    filterProjects('');
+// Function to toggle filter
+function toggleFilter(tech, button) {
+	if (activeTechs.includes(tech)) {
+		// If the same button is clicked again, remove it from the list of active techs
+		activeTechs = activeTechs.filter((t) => t !== tech);
+	} else {
+		// If a different button is clicked, add it to the list of active techs
+		activeTechs.push(tech);
+	}
+	button.classList.toggle("active"); // Toggle active class
+	filterProjects(activeTechs);
+}
+
+// Function to setup project card
+function setupProjectCards(projects) {
+	projects.forEach((project, index) => {
+		const isActive = index === 0 ? "active" : "";
+		const technologies = project.technologies
+			.map((tech) => {
+				if (tech === "fltk") {
+					return "<a>fltk</a>";
+				} else {
+					return `<i class="devicon-${tech}-plain"></i>`;
+				}
+			})
+			.join(" ");
+		const projectCard = `
+                    <div class="carousel-item ${isActive}">
+                        <div class="card mb-4 white-shadow bg-dark-gray text-light">
+                            <div class="card-body">
+                                <h5 class="card-title text-hover">${
+																	project.title
+																} ${
+			project.link
+				? `<a href="${project.link}" target="_blank" class="bi bi-github"></a>`
+				: ""
+		}</h5>
+                
+                                <p class="card-text">${project.description}</p>
+                                <div class="technologies">${technologies}</div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+
+		carouselInner.insertAdjacentHTML("beforeend", projectCard);
+	});
+}
+
+function mapFilters() {
+	const filtersMapping = {};
+	projects.forEach((project) => {
+		project.technologies.forEach((tech, index) => {
+			filtersMapping[tech] = project.filters[index];
+		});
+	});
+
+	return filtersMapping;
+}
+
+// Generate filter buttons as dropdown items
+function dropdownMenuSetup(technologies) {
+	const dropdown = document.createElement("div");
+	const filterButtonsContainer = document.getElementById("filter-buttons");
+	dropdown.className = "dropdown d-inline-block";
+
+	const dropdownButton = document.createElement("button");
+	dropdownButton.className = "btn btn-secondary dropdown-toggle";
+	dropdownButton.type = "button";
+	dropdownButton.id = "dropdownMenuButton";
+	dropdownButton.setAttribute("data-bs-toggle", "dropdown");
+	dropdownButton.setAttribute("aria-expanded", "false");
+	dropdownButton.innerHTML = '<i class="bi bi-funnel"></i>';
+
+	const dropdownMenu = document.createElement("ul");
+	dropdownMenu.className = "dropdown-menu";
+	dropdownMenu.setAttribute("aria-labelledby", "dropdownMenuButton");
+	filtersMap = mapFilters();
+
+	technologies.forEach((tech) => {
+		const dropdownItem = document.createElement("li");
+		const button = document.createElement("button");
+		button.className = "dropdown-item";
+		button.innerText = filtersMap[tech];
+		button.addEventListener("click", () => {
+			toggleFilter(tech, button);
+		});
+		dropdownItem.appendChild(button);
+		dropdownMenu.appendChild(dropdownItem);
+	});
+
+	const divider = document.createElement("div");
+	divider.className = "dropdown-divider";
+	dropdownMenu.appendChild(divider);
+
+	// Create and append the "Clear Filters" button
+	const clearButton = document.createElement("button");
+	clearButton.className =
+		"btn text-danger sm-3 dropdown-item d-flex justify-content-center align-items-center";
+	clearButton.innerHTML = '<i class="bi bi-trash"></i>';
+	clearButton.addEventListener("click", clearFilters);
+	clearButton.addEventListener("touchstart", clearFilters);
+	dropdownMenu.appendChild(clearButton);
+
+	dropdown.appendChild(dropdownButton);
+	dropdown.appendChild(dropdownMenu);
+	filterButtonsContainer.appendChild(dropdown);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	fetch("../json/projects.json")
+		.then((response) => response.json())
+		.then((data) => {
+			globalThis.projects = data;
+
+			// Extract unique technologies
+			const technologies = new Set();
+			projects.forEach((project) => {
+				project.technologies.forEach((tech) => technologies.add(tech));
+			});
+
+			dropdownMenuSetup(technologies);
+			setupProjectCards(projects);
+
+			// Initially display all projects
+			filterProjects("");
+		});
 });
