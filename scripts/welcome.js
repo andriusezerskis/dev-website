11ded9262c7cf8d14fe2af286d5e1af9036cec42
Alex4923 <110432835+Alex4923@users.noreply.github.com>
2024-09-08 02:45:31 +0300

    Utilisation de const pour les variables qui ne changent pas

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,18 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var text = "Welcome to my website, I'm Andrius  ! I'm 20 years old and I'm a computer science student. During my free time, I \
-        love to play video games and code. Feel free to look around!";
-    var delay = 50; // Delay between each character in milliseconds
+document.addEventListener("DOMContentLoaded", () => {
+    const text = "Welcome to my website, I'm Andrius! I'm 20 years old and I'm a computer science student. During my free time, I love to play video games and code. Feel free to look around!";
+    const delay = 50; // Delay between each character in milliseconds
 
-    var welcomeText = document.getElementById("welcome-text");
-    var index = 0;
+    const welcomeText = document.getElementById("welcome-text");
+    let index = 0;
 
-    function typeWriter() {
+    const typeWriter = () => {
         if (index < text.length) {
-            welcomeText.innerHTML += text.charAt(index);
-            index++;
+            welcomeText.innerHTML += text.charAt(index++);
             setTimeout(typeWriter, delay);
         }
-    }
+    };
 
     typeWriter();
 });
