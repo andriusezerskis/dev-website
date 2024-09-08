0a63d21b265da835ba5135589162b9ffcb5dd0ec
andriusezerskis <andriuko6@gmail.com>
2024-09-08 17:17:55 +0200

    🚧 trying to fix the header import

--- scripts/header_import.js
+++ scripts/header_import.js
@@ -1,8 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Import header
-    fetch('main_header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header-placeholder').innerHTML = data;
-        });
+function loadHTML(url, elementId, callback) {
+	fetch(url)
+		.then((response) => response.text())
+		.then((data) => {
+			document.getElementById(elementId).innerHTML = data;
+			if (callback) callback();
+		})
+		.catch((error) => console.error("Error loading HTML:", error));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	/*
+        // Import header
+        fetch("main_header.html")
+            .then((response) => response.text())
+            .then((data) => {
+                document.getElementById("header-placeholder").innerHTML = data;
+                // Import header JS
+                const script = document.createElement("script");
+                script.src = "../scripts/main_header.js";
+                document.head.appendChild(script);
+            });
+    */
+
+	// Load the navigation bar into the placeholder div
+	loadHTML("main_header.html", "header-placeholder", function () {
+		// This code runs after the HTML content is loaded
+		const script = document.createElement("script");
+		script.src = "../scripts/main_header.js";
+		script.defer = true;
+		document.body.appendChild(script);
+	});
 });
--- scripts/main_header.js
+++ scripts/main_header.js
@@ -1,11 +1,16 @@
-document
-	.getElementById("discord-icon")
-	.addEventListener("click", function (event) {
-		event.preventDefault(); // Prevent the default action of the link
-		const discordInfo = document.getElementById("discord-info");
-		if (discordInfo.style.display === "none") {
-			discordInfo.style.display = "block";
-		} else {
-			discordInfo.style.display = "none";
-		}
-	});
+function copyToClipboard() {
+	// Get the text field
+	var copyText = document.getElementById("discord-info-modal");
+
+	// Copy the text inside the text field
+	navigator.clipboard.writeText(copyText.innerText);
+	console.log("Copied the text: " + copyText.innerText);
+	// Alert the copied text
+	alert("Copied the text: " + copyText.innerText);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	console.log("Loaded");
+	copyButton = document.getElementById("copy-btn");
+	copyButton.addEventListener("click", copyToClipboard);
+});
